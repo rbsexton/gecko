c9197915cb8d6c143ada2583c07fd421e4b1136a
rbsexton <robertbsexton@gmail.com>
2015-03-07 08:20:57 -0800

    Baseline code.

---
+++ zero/timers/launcher/interconnect.c
@@ -0,0 +1,22 @@
+// Interconnect between C and forth 
+// Two parts -
+//  Jump table of C functions that would be useful for calling from forth.
+//  A small structure pinned to the very beginning of memory. 
+// You could potentially use some reserved vectors, but not all Cortex-M
+// devices but the reset vector in the same spot.  Thanks ST :-(
+
+#include <stdint.h>
+
+#include "interconnect.h"
+
+tSharedData theshareddata __attribute__ ((section(".shareddata"))) = {
+	0,0
+ 	};
+
+// Lie!
+extern void EMU_EnterEM2();
+
+void (* const jumptable[])(void) __attribute__ ((section(".jumptable"))) = {
+	EMU_EnterEM2,
+	};
+
---
+++ zero/timers/launcher/launcher.c
@@ -0,0 +1,253 @@
+/**************************************************************************//**
+ * @file
+ * @brief LEUART Demo Application
+ * @author Energy Micro AS
+ * @version 1.07
+ ******************************************************************************
+ * @section License
+ * <b>(C) Copyright 2013 Energy Micro AS, http://www.energymicro.com</b>
+ *******************************************************************************
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ * 4. The source and compiled code may only be used on Energy Micro "EFM32"
+ *    microcontrollers and "EFR4" radios.
+ *
+ * DISCLAIMER OF WARRANTY/LIMITATION OF REMEDIES: Energy Micro AS has no
+ * obligation to support this Software. Energy Micro AS is providing the
+ * Software "AS IS", with no express or implied warranties of any kind,
+ * including, but not limited to, any implied warranties of merchantability
+ * or fitness for any particular purpose or warranties against infringement
+ * of any proprietary rights of a third party.
+ *
+ * Energy Micro AS will not be liable for any consequential, incidental, or
+ * special damages, or any other relief, or for any claim by any third party,
+ * arising from your use of this Software.
+ *
+ *****************************************************************************/
+#include "em_chip.h"
+#include "em_device.h"
+#include "em_cmu.h"
+#include "em_emu.h"
+#include "em_leuart.h"
+#include "em_dma.h"
+#include "em_gpio.h"
+#include "em_rtc.h"
+
+#include "bl_launcher.h"
+#include "interconnect.h"
+
+/* DEFINES */
+#define WAKEUP_INTERVAL_MS    5000
+
+/* GLOBAL VARIABLES */
+
+uint32_t rtcCountBetweenWakeup;
+
+/* Defining the LEUART1 initialization data */
+LEUART_Init_TypeDef leuart0Init =
+{
+  .enable   = leuartEnable,       /* Activate data reception on LEUn_TX pin. */
+  .refFreq  = 0,                    /* Inherit the clock frequenzy from the LEUART clock source */
+  .baudrate = 9600,                 /* Baudrate = 9600 bps */
+  .databits = leuartDatabits8,      /* Each LEUART frame containes 8 databits */
+  .parity   = leuartNoParity,       /* No parity bits in use */
+  .stopbits = leuartStopbits1,      /* Setting the number of stop bits in a frame to 2 bitperiods */
+};
+
+/* Set up RTC init struct*/
+RTC_Init_TypeDef rtcInit =
+{
+  .debugRun = false,
+  .comp0Top = true,
+  .enable   = true,
+};
+
+// ------------- Statistics ------------------
+int count_leuart_irqs = 0;
+
+// ******************* Boot Banner ****************************
+const char message[] = "Boot! ";
+void SayHello() {
+	const char *p = message;
+	while(*p) LEUART_Tx(LEUART0,*p++);
+	}
+	
+#if 0 
+	while(*p) {
+		if ( LEUART0->STATUS & LEUART_STATUS_TXBL ) {
+			LEUART_Tx(LEUART0,*p++);
+			}
+		else { 
+    		EMU_EnterEM2(false); // Using 'True' increased power.
+			}
+		
+	}	
+}
+#endif
+
+/**************************************************************************//**
+ * @brief RTC Interrupt Handler.
+ *
+ * This routine will run every other second, when the RTC times out, and generate
+ * an interrupt. After clearing the interrupt source, the DMA source address is
+ * set, and a new DMA transfer is initiated. When the routine is finished, the
+ * system will again enter EM2 while the DMA continues to transfer data from the
+ * memory to the LEUART. The LEUART DMA wake-up on TX is enabled to allow a 
+ * LEUART DMA request to wake up the DMA. 
+ *
+ *****************************************************************************/
+void RTC_IRQHandler(void)
+{
+  /* Clear interrupt source */
+  RTC_IntClear(RTC_IFC_COMP0);
+}
+
+/**************************************************************************//**
+ * @brief  Initialize Low Energy UART 1
+ *
+ * Here the LEUART is initialized with the chosen settings. It is then routed
+ * to location 0 to avoid conflict with the LCD pinout. Finally the GPIO mode
+ * is set to push pull.
+ *
+ *****************************************************************************/
+
+// For the Zero Gecko board, TX=PD4, RX=PD5
+
+void initLeuart(void)
+{
+  /* Reseting and initializing LEUART1 */
+  LEUART_Reset(LEUART0);
+  LEUART_Init(LEUART0, &leuart0Init);
+
+  /* Route LEUART0 TX pin to DMA location 0 */
+  LEUART0->ROUTE = LEUART_ROUTE_TXPEN | LEUART_ROUTE_RXPEN |
+                   LEUART_ROUTE_LOCATION_LOC0;
+
+  /* Enable TX Completion and RX Data */
+  LEUART_IntEnable(LEUART0, LEUART_IEN_RXDATAV);
+
+  /* Enable GPIO for LEUART0. TX is on C6 */
+  GPIO_PinModeSet(gpioPortD,                /* GPIO port */
+                  4,                        /* GPIO port number */
+                  gpioModePushPull,         /* Pin mode is set to push pull */
+                  1);                       /* High idle state */
+
+  GPIO_PinModeSet(gpioPortD,                /* GPIO port */
+                 5,                        /* GPIO port number */
+                 gpioModeInputPull,         /* Pin mode is set to push pull */
+                 1);                       /* High idle state */
+
+  /* Enable RTC interrupt vector in NVIC */
+  NVIC_EnableIRQ(LEUART0_IRQn);
+
+}
+
+// Check for valid data, and if so clear it by pulling it out.
+void LEUART0_IRQHandler(void) {
+	/* Store and reset pending interupts */
+	uint32_t leuartif = LEUART_IntGet(LEUART0);
+ 	LEUART_IntClear(LEUART0, leuartif);
+  
+	if ( leuartif & LEUART_IEN_RXDATAV ) {
+		theshareddata.u0rxdata = LEUART0->RXDATAX;
+	}
+  	count_leuart_irqs++;
+}
+
+/**************************************************************************//**
+ * @brief  Setup Real Time Clock (RTC)
+ *
+ * The RTC is initialized, and is set up to generate an interrupt every other
+ * second.
+ *
+ *****************************************************************************/
+void setupRtc(void)
+{
+  /* Input RTC init struct in initialize funciton */
+  RTC_Init(&rtcInit);
+
+  /* Set RTC compare value */
+  rtcCountBetweenWakeup = ((SystemLFXOClockGet() * WAKEUP_INTERVAL_MS) / 1000);
+  RTC_CompareSet(0, rtcCountBetweenWakeup);
+
+  /* Enable RTC interrupt from COMP0 */
+  RTC_IntEnable(RTC_IF_COMP0);
+
+  /* Enable RTC interrupt vector in NVIC */
+  NVIC_EnableIRQ(RTC_IRQn);
+
+  /* Enable RTC */
+  RTC_Enable(true);
+}
+
+// See if there is a character in the LEUART buffer and echo it back.
+
+void CheckandEcho() {
+	if ( LEUART0->STATUS & LEUART_STATUS_RXDATAV ) {
+		LEUART0->TXDATA = LEUART0->RXDATA;
+		
+		// LEUART_Tx(LEUART0,LEUART0->RXDATA);
+	}
+}
+
+/**************************************************************************//**
+ * @brief  Main function
+ *
+ * This example demonstrates a way to use the Low Energy UART to maintain full
+ * UART communication capabilities, while spending a great majority of the time
+ * in deep sleep mode EM2. The LEUART is in this example driven by the LFXO,
+ * which provide good accuracy while consuming only small amounts of energy. In
+ * addition the DMA is set up to read the data to be transmitted by the LEUART
+ * directly from the system memory. This relieves the CPU from doing anything
+ * other than initializing the transfer, and handle possible interrupts triggered
+ * when the transfer is finished. In this case the strings "HELLO" and "THERE"
+ * are alternatingly transmitted every other second through the LEUART.
+ *
+ *****************************************************************************/
+int main(void)
+{
+
+  theshareddata.jumptable = (uint32_t) &jumptable; // Initialize it at runtime 
+
+  /* Initialize chip */
+  CHIP_Init();
+
+  /* Start LFXO, and use LFXO for low-energy modules */
+  CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_LFXO);
+  CMU_ClockSelectSet(cmuClock_LFB, cmuSelect_LFXO);
+
+  /* Enabling clocks, all other remain disabled */
+  CMU_ClockEnable(cmuClock_CORELE, true);     /* Enable CORELE clock */
+  CMU_ClockEnable(cmuClock_GPIO, true);       /* Enable GPIO clock */
+  CMU_ClockEnable(cmuClock_LEUART0, true);    /* Enable LEUART0 clock */
+  CMU_ClockEnable(cmuClock_RTC, true);        /* Enable RTC clock */
+
+  /* Re-config the HFRCO to the low band */
+  CMU_HFRCOBandSet(cmuHFRCOBand_1MHz); 
+
+  /* Initialize LEUART */
+  initLeuart();
+
+  /* Setup RTC as interrupt source */
+  setupRtc();
+
+  SayHello();
+
+LaunchUserAppNoNVIC( (long unsigned int *) 0x2000);
+
+  // Lets just do echo.
+  while (1)
+  {
+	CheckandEcho();
+    /* Enable deep sleep */
+    // EMU_EnterEM2(false);
+  }
+}
