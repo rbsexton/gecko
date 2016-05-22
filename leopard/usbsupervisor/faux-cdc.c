682e04982a1465a7a6987cccd6a0df4cb4b07cf8
rbsexton <robertbsexton@gmail.com>
2016-05-21 19:47:20 -0700

    Purge so irrelevant comments.

--- leopard/usbsupervisor/faux-cdc.c
+++ leopard/usbsupervisor/faux-cdc.c
@@ -104,28 +104,6 @@ unsigned count_syscalls_eol[2];
 // The CDC driver has a Rx timeout functionality which require a timer.
 #define CDC_TIMER_ID ( 0 )
 
-// DMA related macros, select DMA channels and DMA request signals.
-#define CDC_UART_TX_DMA_CHANNEL   ( 0 )
-#define CDC_UART_RX_DMA_CHANNEL   ( 1 )
-#define CDC_TX_DMA_SIGNAL         DMAREQ_UART1_TXBL
-#define CDC_RX_DMA_SIGNAL         DMAREQ_UART1_RXDATAV
-
-// UART/USART selection macros.
-#define CDC_UART                  UART1
-#define CDC_UART_CLOCK            cmuClock_UART1
-#define CDC_UART_ROUTE            ( UART_ROUTE_RXPEN | UART_ROUTE_TXPEN | \
-                                    UART_ROUTE_LOCATION_LOC2 )
-#define CDC_UART_TX_PORT          gpioPortB
-#define CDC_UART_TX_PIN           9
-#define CDC_UART_RX_PORT          gpioPortB
-#define CDC_UART_RX_PIN           10
-
-// Activate the RS232 switch on DK's.
-#define CDC_ENABLE_DK_UART_SWITCH() BSP_PeripheralAccess(BSP_RS232_UART, true)
-
-// No RS232 switch on STK's. Leave the definition "empty".
-#define CDC_ENABLE_DK_UART_SWITCH()
-
   @endverbatim
  ** @} ***********************************************************************/
 
