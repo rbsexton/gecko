6f70913d86545a34b9b03f5d2a6b25aed3e6eca8
rbsexton <robertbsexton@gmail.com>
2017-06-14 00:36:48 -0700

    Splice in the handlers for the leuart console.

--- leopard/usbsupervisor/systemcalls.c
+++ leopard/usbsupervisor/systemcalls.c
@@ -10,6 +10,7 @@
 #include "syscalls.h"
 #include "usb-sapi.h"
 
+#include "console_leuart.h"
 
 long* __SAPI_01_GetRuntimeData(int i) {
 	if ( i == 0 ) return(0);
@@ -21,10 +22,12 @@ extern RINGBUF rb_IN;
 /// @parameters
 /// @R0 - Stream Number
 /// @R1 - The Character in question.
-/// @returns in R0 - Result - 0 for success. -1 for highwater, -2 for fail
-int __SAPI_02_PutChar(int stream, uint8_t c, unsigned long *tcb) {
+/// @returns in R0 - Result - 0 for success. 1 for blocked - Thread must yield/pause
+bool __SAPI_02_PutChar(int stream, uint8_t c, unsigned long *tcb) {
 	int ret;
 	switch ( stream ) {
+		case 0: 
+			return(console_leuart_putchar(c, tcb));
 		case 10:
 		case 11:
 			return(USBPutChar(stream-10, c));
@@ -35,8 +38,11 @@ int __SAPI_02_PutChar(int stream, uint8_t c, unsigned long *tcb) {
 	return(ret);
 	}
 
+// Support blocking by returning -1.
 int __SAPI_03_GetChar(int stream, unsigned long *tcb) {
 	switch ( stream ) {
+		case 0:
+			return(console_leuart_getchar());
 		case 10:
 		case 11:
 			return(USBGetChar(stream-10, tcb));			
@@ -46,8 +52,11 @@ int __SAPI_03_GetChar(int stream, unsigned long *tcb) {
 		}
 	}
 
+// Return the number of incoming characters.
 int __SAPI_04_GetCharAvail(int stream) {
 	switch ( stream ) {
+		case 0:
+			return(console_leuart_charsavailable());
 		case 10:
 		case 11:
 			return(USBGetCharAvail(stream-10));
@@ -57,7 +66,9 @@ int __SAPI_04_GetCharAvail(int stream) {
 		}
 	}
 
-int __SAPI_05_PutString(int stream,  int len, uint8_t *p, unsigned long *tcb) {
+// Putstring - System call to support TYPE.
+// Return 0 for success
+bool __SAPI_05_PutString(int stream,  int len, uint8_t *p, unsigned long *tcb) {
 	switch ( stream ) {
 		case 10:
 		case 11:
@@ -69,8 +80,10 @@ int __SAPI_05_PutString(int stream,  int len, uint8_t *p, unsigned long *tcb) {
 	}
 
 // Stubbed out.
-int __SAPI_06_EOL(int stream, unsigned long *tcb) {
+bool __SAPI_06_EOL(int stream, unsigned long *tcb) {
 	switch ( stream ) {
+		case  0:
+			return(USBOutEOL(stream-10,tcb));
 		case 10:
 		case 11:
 			return(USBOutEOL(stream-10,tcb));
