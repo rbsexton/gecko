24d7ff36410dfa24dcfab45c6ed56f2d9a6f90f2
rbsexton <robertbsexton@gmail.com>
2017-06-28 22:05:40 -0700

    GCC doesn’t really understand binary files, so turn the packaged binary into a .elf file.

--- tiny/basic/build.sh
+++ tiny/basic/build.sh
@@ -4,6 +4,8 @@ LAUNCHSIZE=$(( 2 * 4096 ))
 FIRSTBINARY=supervisor/exe/supervisor.bin
 SECONDBINARY=forth/TINY.img
 
+# arm-none-eabi-objcopy -O elf32-littlearm -B arm --rename-section .data=.text -I binary forth/TINY.img forth.o
+
 cd supervisor; make;
 cd .. 
 
@@ -24,4 +26,12 @@ echo "$FIRSTBINARY($LEN) + $PAD + $SECONDBINARY($LEN2) = $TOT"
 	dd if=/dev/zero bs=1 count=$PAD;
 	cat $SECONDBINARY;
 } > packaged.bin
+
+# Generate a .o file for use with gdb & the Black Magic probe.
+arm-none-eabi-objcopy -O elf32-littlearm \
+	-B arm\
+	--rename-section .data=.text\
+	-I binary packaged.bin\
+	packaged.elf
+
 
